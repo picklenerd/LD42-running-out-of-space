748932a647acef891ef6b81bddebfd0b8e8b07fb
Jordan Grace <jordan.t.grace@gmail.com>
2018-08-12 13:49:14 -0400

    Enemies spawn off screen now

--- src/systems/enemy.rs
+++ src/systems/enemy.rs
@@ -31,7 +31,7 @@ impl EnemySystem {
         state.pixi().add_child(&enemy_circle);
         
         let enemy = state.ecs().create_entity();
-        let start_pos = self.get_spawn_position(state);
+        let start_pos = self.get_spawn_position();
         let _ = state.ecs().set(enemy, start_pos.clone());
         let _ = state.ecs().set(enemy, Velocity{x: 0.0, y: 0.0});
         let _ = state.ecs().set(enemy, Enemy);
@@ -41,22 +41,11 @@ impl EnemySystem {
         let _ = state.ecs().set(enemy, Slowable::new(constants::ENEMY_SLOWED_MULTIPLIER));
     }
 
-    fn get_spawn_position(&mut self, state: &mut GameState) -> Position { 
-        let mut player_ids = Vec::new();
-        state.ecs().collect_with(&component_filter!(Player), &mut player_ids);
-        let pos = state.ecs().get::<Position>(player_ids[0]).unwrap();
-
-        let mut x = pos.x;
-        let mut y = pos.y;
-
-        while (x - pos.x).abs() < constants::ENEMY_MIN_SPAWN_DISTANCE {
-            x = self.rng.gen_range(0.0, constants::SCREEN_WIDTH as f64);
-        }
-        while (y - pos.y).abs() < constants::ENEMY_MIN_SPAWN_DISTANCE {
-            y = self.rng.gen_range(0.0, constants::SCREEN_HEIGHT as f64);
-        }
-        
-        Position{x, y}
+    fn get_spawn_position(&mut self) -> Position { 
+        let x: i32 = self.rng.gen_range(0, 2) * constants::SCREEN_WIDTH as i32;
+        let y: i32 = self.rng.gen_range(0, 2) * constants::SCREEN_HEIGHT as i32;
+      
+        Position{x: x as f64, y: y as f64}
     }
 }
 
